3c86c13ad5c050c44aa2e01017dca0145546236f
Major Issue <sound_700@htomail.com>
2022-11-14 22:22:33 +0200

    Tutorial started we skipped many stuff
    Signed-off-by: Major Issue <sound_700@htomail.com>

---
+++ NodeNinjaTut/modules.js
@@ -0,0 +1,4 @@
+const xyz = require('./people')
+console.log(xyz.people,xyz.ages)
+const os = require('os')
+console.log(os)
---
+++ NodeNinjaTut/people.js
@@ -0,0 +1,7 @@
+const people = ['jack','fred','douch']
+const ages = [23,24,53]
+// console.log(people)
+module.exports ={
+    people : people,
+    ages : ages
+} 
